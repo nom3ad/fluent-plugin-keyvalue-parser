6ee512535692ad4e9052f796da2cf6e1446965de
arun.muraleedharan <arunmj001@gmail.com>
2017-01-20 15:56:06 +0530

    added unparsed last key append

--- lib/fluent/plugin/parser_jsonify.rb
+++ lib/fluent/plugin/parser_jsonify.rb
@@ -4,46 +4,75 @@ require 'fluent/time'
 
 
 module Fluent
-  class TextParser
-    class JsonKeyValueParser < Parser
+	class TextParser
+		class JsonKeyValueParser < Parser
 
-      # Register this parser as "jsonify"
-      Plugin.register_parser("jsonify", self)
+			# Register this parser as "jsonify"
+			Plugin.register_parser("jsonify", self)
 
-      config_param :pair_delimiter, :string, :default => " " 
-      config_param :key_value_seperator, :string, :default => "," 
+			config_param :pair_delimiter, :string, :default => " " 
+			config_param :key_value_seperator, :string, :default => "," 
 
-      def configure(conf)
+			def configure(conf)
 
-        super
+			super
 
-        #  if @pair_delimiter.length != 1
-        #  raise ConfigError, "delimiter must be a single character. #{@delimiter} is not."
-        # end
+			#  if @pair_delimiter.length != 1
+			#  raise ConfigError, "delimiter must be a single character. #{@delimiter} is not."
+			# end
 
-      end
+			end
 
-      def parse(text)
-        $log.debug "recieved text = " + text
-        kv_pairs = text.split(@pair_delimiter)
-        $log.debug "kv_pairs  = " + kv_pairs.to_s
-        record = {}
-        key_construct = ""
-        kv_pairs.each { |kv|
-          k, v = kv.split(@key_value_seperator, 2)
-          key_construct << k
-          if v.nil?
-            key_construct << @pair_delimiter
-          else
-            $log.debug "full key ="+key_construct
-            record[key_construct] = v
-            key_construct = ""
-          end
-        }
-        record[record.keys.last] << (@pair_delimiter + key_construct)
-        # $log.debug "gonna emit this record: " + record.to_s + " when ticks at " + time.to_s
-        yield nil, record
-      end
-    end
-  end
+			def parse(text)
+				record = {}
+				tokens = text.split(@pair_delimiter)
+				is_quote_open = false
+				key_name = ""
+				value = ""
+				tokens.each_with_index do |item,idx|
+					if item.split(@key_value_seperator,2)[1].nil?
+
+						if is_quote_open
+							if item[-1] == "\""
+								#puts "end quote_found" + " at " + item
+								value << (@pair_delimiter + item[0..-2])
+								is_quote_open = false
+								record[key_name] = value
+								value = "";key_name = ""
+							else
+								value << (@pair_delimiter + item)
+							end
+						else
+							key_name << (item + @pair_delimiter)
+						end
+
+					else
+						k,v = item.split(@key_value_seperator,2)
+						key_name << k
+						if v[0] == "\""
+							is_quote_open = true
+							if v[-1] == "\""
+								is_quote_open = false
+								value << v[1..-2]
+								record[key_name] = value
+								value = "";key_name = ""
+							else
+								value << v[1..-1]
+							end
+						else
+							is_quote_open = false
+							value << v
+							record[key_name] = value
+							value = "";key_name = ""
+						end
+					end
+				end
+
+				# append unparsed tail item to last key
+				record[record.keys.last] << (@pair_delimiter + key_name)
+				#$log.debug "gonna emit this record: " + record.to_s + " when ticks at " + time.to_s
+				yield nil, record
+			end
+		end
+	end
 end
